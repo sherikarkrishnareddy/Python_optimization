56f9f2255ce96f6e0d34b02457434417253a1001
sherikarkrishnareddy <54585309+sherikarkrishnareddy@users.noreply.github.com>
2021-09-20 14:58:12 +0530

    Python multiprocessing example
    Output of the script :
    
    Macbooks-MBP:Python_optimization macbook$ /usr/local/bin/python3 /Users/macbook/Documents/Python_optimization/multiprocessing_sample.py
    Outputs : <map object at 0x7fa92ed14c40>
    Starting SpawnPoolWorker-1
    Starting SpawnPoolWorker-2
    Starting SpawnPoolWorker-3
    Starting SpawnPoolWorker-4
    Starting SpawnPoolWorker-6
    Starting SpawnPoolWorker-5
    Starting SpawnPoolWorker-7
    Starting SpawnPoolWorker-8
    Macbooks-MBP:Python_optimization macbook$

---
+++ multiprocessing_sample.py
@@ -0,0 +1,15 @@
+import multiprocessing
+from multiprocessing.spawn import _main
+def do_task(data):
+    return data*2
+def start_process():
+    print('Starting',multiprocessing.current_process().name)
+if __name__=='__main__':
+    pool_size=multiprocessing.cpu_count()*2
+    pool=multiprocessing.Pool(processes=pool_size,initializer=start_process)
+    inputs=list(range(10))
+    outputs=map(do_task,inputs)
+    print('Outputs :',outputs)
+    pool.close()
+    pool.join()
+    
